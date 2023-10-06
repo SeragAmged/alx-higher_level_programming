e16d301d943ea2bf6d597b3517ee04713ca2c21a
SeragAmged <seragamged2002@gmail.com>
2023-10-06 03:17:37 +0300

    args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the number of and list of arguments."""
+    import sys
+    if sys.argv == 1:
+        print("0 arguments.")
+    else:
+        print(f"{len(sys.argv) - 1:d} arguments:")
+        for index, arg in enumerate(sys.argv):
+            if index == 0:
+                continue
+            elif index + 1 == len(sys.argv):
+                print(f"{index}: {arg}", end="")
+            else:
+                print(f"{index}: {arg}")
