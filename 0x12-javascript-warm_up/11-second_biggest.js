7bd2a343679270f59597222a4ced0cc132115f1a
SeragAmged <seragamged2002@gmail.com>
2024-01-10 23:21:47 +0200

    gg

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,14 @@
 #!/usr/bin/node
-let arr = process.argv.slice(2);
-arr.forEach(function (element, index) {
-    arr[index] = Number(element);
-})
-console.log(arr.sort((a, b) => b - a)[1]);
+
+if (process.argv.length <= 2) {
+    console.log(0);
+} else {
+
+    let arr = process.argv.slice(2);
+    arr.forEach(function (element, index) {
+        arr[index] = Number(element);
+    })
+    console.log(arr.sort((a, b) => b - a)[1]);
+}
+
+
