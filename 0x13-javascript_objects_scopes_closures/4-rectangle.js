573c28c74c8a3caf7003a56ed8b6554c685d27ec
SeragAmged <seragamged2002@gmail.com>
2024-01-11 01:49:28 +0200

    ff

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print () {
+    for (let index = 0; index < this.height; index++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+  rotate () {
+    let temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+}
+module.exports = Rectangle;
